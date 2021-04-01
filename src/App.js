bc88788e864a960510f1f457d9177456a39ac932
Ashish Bassi <ashish.bassi@statflo.com>
2021-04-01 09:01:36 -0400

    Added Task JSON

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }
+  ])
+
+  const deleteTask = (id) => {
+    console.log('Deleting Task ', id);
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    
+    console.log('Toggle Reminder' , id);
+    setTasks(tasks.map((task) => 
+      task.id === id ?
+        {...task, reminder: !task.reminder } :task))
+  }
+
   return (
     <div className='container'>
       <Header title="Task Tracker from app.js"></Header>
-      <Tasks/>
+      
+      { tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks available' 
+      }
     </div>
   );
 }
